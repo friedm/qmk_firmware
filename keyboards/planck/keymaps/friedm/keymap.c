1236db78f3775974e282f9db41359ecc5ce2b395
Matt Friedman <matt@friedm.com>
2018-07-08 12:59:07 -0700

    update symbol layout

--- keyboards/planck/keymaps/friedm/keymap.c
+++ keyboards/planck/keymaps/friedm/keymap.c
@@ -45,39 +45,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-------------------------------------------------------------------------------------.
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Back |
  * |--------+------+------+------+------+-------------+------+------+------+------+------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |--------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RShift|
  * |--------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite  | Bksp | Super| Alt  |Navi  |    Space    |Symb  | Home | PgDn | PgUp |End   |
+ * | Brite  | Bksp | Super| Alt  |  Navi| Space/Navi  |Symb  | Ins  |      |      |      |
  * `-------------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  {BACKLIT,        KC_BSPC, KC_LGUI, KC_LALT, NAVI,    KC_SPC,  KC_SPC,  SYMB,    KC_HOME, KC_PGDN, KC_PGUP, KC_END}
+  {BACKLIT,        KC_BSPC, KC_LGUI, KC_LALT, NAVI,  LT(NAVI,KC_SPC),  LT(NAVI,KC_SPC),  SYMB,    KC_INS, KC_PGDN, KC_PGUP, KC_END}
 },
 
 
 /* Lower (Symb)
  * ,-----------------------------------------------------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   -  |   =  |  +   |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |  \   |  (   |  )   |   [  |  ]   |   |  |  0   |   1  |  2   |   3  |   4  |  '   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  <   |  >   |   {  |  }   |   _  |  5   |   6  |  7   |   8  |   9  |  "   |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   _  |   -  |   =  |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |  <   |  (   |   '  |  )   |   >  |  0   |   1  |  2   |   3  |   4  |  |   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   `  |  [   |  {   |   "  |  }   |   ]  |  5   |   6  |  7   |   8  |   9  |  *   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ |  \   |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_MINS, KC_EQL, KC_PLUS},
-  {KC_BSLS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_PIPE, KC_0,    KC_1,       KC_2,       KC_3,    KC_4,   KC_QUOT},
-  {_______, KC_LT,   KC_GT,   KC_LCBR, KC_RCBR, KC_UNDS, KC_5,    KC_6,       KC_7,       KC_8,    KC_9,   KC_DQT},
-  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_UNDS,    KC_MINS, KC_EQL, KC_PLUS},
+  {_______, KC_LT,   KC_LPRN, KC_QUOT, KC_RPRN, KC_GT,   KC_0,    KC_1,       KC_2,       KC_3,    KC_4,   KC_PIPE},
+  {KC_GRV,  KC_LBRC, KC_LCBR, KC_DQT,  KC_RCBR, KC_RBRC, KC_5,    KC_6,       KC_7,       KC_8,    KC_9,   KC_ASTR},
+  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU,KC_BSLS}
 },
 
 /* Raise (Navi)
