6d77f1a517d76014970d951a38e990fae460d7f1
Matt Friedman <matt@friedm.com>
2018-07-09 06:44:20 -0700

    swap planck layer keys

--- keyboards/planck/keymaps/friedm/keymap.c
+++ keyboards/planck/keymaps/friedm/keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RShift|
  * |--------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite  | Bksp | Super| Alt  |  Navi| Space/Navi  |Symb  | Ins  |      |      |      |
+ * | Brite  | Bksp | Super| Alt  |Spc/Nv|  Ins | Back | Symb |      |      |      |      |
  * `-------------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  {BACKLIT,        KC_BSPC, KC_LGUI, KC_LALT, NAVI,  LT(NAVI,KC_SPC),  LT(NAVI,KC_SPC),  SYMB,    KC_INS, KC_PGDN, KC_PGUP, KC_END}
+  {BACKLIT,        KC_BSPC, KC_LGUI, KC_LALT, LT(NAVI,KC_SPC),  KC_INS,  KC_BSPC, SYMB, KC_NO,   KC_NO,   KC_NO,   KC_NO}
 },
 
 
